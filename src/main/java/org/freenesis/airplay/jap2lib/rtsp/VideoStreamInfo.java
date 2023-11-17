f69267386b585215dc3cae894302ef373aeed89d
Pranav M S <pranavms13@yahoo.com>
2023-11-17 06:13:30 +0530

    Initial

---
+++ src/main/java/org/freenesis/airplay/jap2lib/rtsp/VideoStreamInfo.java
@@ -0,0 +1,19 @@
+package org.freenesis.airplay.jap2lib.rtsp;
+
+public class VideoStreamInfo implements MediaStreamInfo {
+
+    private final String streamConnectionID;
+
+    public VideoStreamInfo(String streamConnectionID) {
+        this.streamConnectionID = streamConnectionID;
+    }
+
+    @Override
+    public StreamType getStreamType() {
+        return MediaStreamInfo.StreamType.VIDEO;
+    }
+
+    public String getStreamConnectionID() {
+        return streamConnectionID;
+    }
+}
